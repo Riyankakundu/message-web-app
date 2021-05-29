82d4e672ea05db5933ead0af0563b37fa6d1a93d
Riyanka Kundu <riyankakundu@gmail.com>
2021-05-29 19:19:55 +0530

    added frontend files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MessagesComponent } from './messages.component';
+import { NewMessageComponent } from './new-message.component';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello {{title}}</h1><messages></messages>',
+  template: `
+    <h1>Message Board</h1>
+    <new-message (onPosted)= "onPosted($event)"></new-message>
+    <messages></messages>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my app';
+  @ViewChild(MessagesComponent) messages: any;
+   onPosted(message:any) {
+     this.messages.messages.push(message);
+   }
 }
